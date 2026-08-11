90520fdb7a6f8c52e7e4c11df40253ce90b025c8
Luis Mercado <luis2412076@hybridge.education>
2026-08-11 15:49:41 -0600

    Update obtenerNombre function to return a name

--- ejercicio01.js
+++ ejercicio01.js
@@ -1,8 +1,10 @@
 // Ejercicio 1: Declaración de Función
-// Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. La función debe retornar esta nueva variable que contiene tu nombre
+// Modifica la siguiente función creando una nueva variable llamada nombre y asignándole tu nombre. 
+//La función debe retornar esta nueva variable que contiene tu nombre
 
 function obtenerNombre() {
-    // Escribe tu código aquí
+    const nombre = "Luis";
+    return nombre;
 }
 
 // No modificar.

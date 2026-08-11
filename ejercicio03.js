bfa253810e0c79451e0c581d6b596f24debab188
Luis Mercado <luis2412076@hybridge.education>
2026-08-11 17:27:35 -0600

    Update ejercicio03.js

--- ejercicio03.js
+++ ejercicio03.js
@@ -2,6 +2,13 @@
 // Declara una función llamada "sumar" que acepte dos parámetros (a y b) y retorne su suma
 
 //Escribe tu función aquí
+function sumar(a, b) {
+    return a + b;
+}
+
+// Ejemplo de uso
+console.log(sumar(5, 3)); // Debería imprimir 8
+console.log(sumar(-2, 7)); // Debería imprimir 5
 
 
 // Exporta la función para que pueda ser utilizada en las pruebas

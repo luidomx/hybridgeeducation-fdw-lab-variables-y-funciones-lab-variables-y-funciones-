37cd44525ba4db6d61873a958d2f8b8b31c11098
Luis Mercado <luis2412076@hybridge.education>
2026-08-11 17:42:27 -0600

    Update ejercicio08.js

--- ejercicio08.js
+++ ejercicio08.js
@@ -6,7 +6,14 @@
 // concatenarCadenas("Frase", "de prueba") => "Frasede prueba"
 
 //escribe aquí tu función. Recuerda que el nombre de la función debe ser exactamente "concatenarCadenas"
+function concatenarCadenas(cadena1, cadena2) {
+    // Retorna la concatenación de las dos cadenas
+    return cadena1 + cadena2;
+}
 
+// Pruebas para verificar que la función funciona correctamente
+console.log(concatenarCadenas("Hola, ", "soy luis")); // Debería imprimir: "Hola, soy luis"
+console.log(concatenarCadenas("Wake up, ", "Neo")); // Debería imprimir: "Wake up, Neo"
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
     concatenarCadenas
